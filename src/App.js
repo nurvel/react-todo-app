9fe9f3c4e18bdda31c31f24846c29fabcadeb58d
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2020-02-20 16:52:55 +0200

    get data from server and sets toapp state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Filter from "./components/Filter";
 
+import { getTodos } from "./services/todoService";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodos()
+      .then(t => setTodos(t))
+      .catch(err => console.log(err));
+  }, []);
+
+  console.log(todos);
+
   return (
     <div>
       <h1>Redux todo app</h1>
       <NewTodo />
-      <Todos />
+      <Todos todos={todos} />
       <Filter />
     </div>
   );
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,6 +2,9 @@ import React from "react";
 // import { connect } from "react-redux";
 
 const Todos = props => {
+
+
+
   return <div>Here will be the Todos</div>;
 };
 
