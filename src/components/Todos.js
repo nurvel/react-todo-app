b28c12365117426310f00f234ae5376d8f770cf0
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2020-02-21 10:03:50 +0200

    render todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,10 +2,36 @@ import React from "react";
 // import { connect } from "react-redux";
 
 const Todos = props => {
+  const maketodoRows = () => {
+    //const { todos } = this.props;
+    return props.todos.map((todo, i) => {
+      console.log(todo);
+      console.log("TODOD", props.todos);
+      return (
+        <tr key={i}>
+          <td>{i}</td>
+          <td>{todo.content}</td>
+          <td>{todo.important.toString()}</td>
+        </tr>
+      );
+    });
+  };
 
-
-
-  return <div>Here will be the Todos</div>;
+  return (
+    <div>
+      <h2>Todos</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Todo</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>{maketodoRows()}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Todos;
