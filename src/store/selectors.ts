ad7f9546b6802458eaf400fbe6dbc06f8bf86a63
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2020-04-11 19:09:10 +0300

    selector refactoring

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,14 +1,18 @@
 import { AppState } from "./index";
 import { Todo } from "./todos/todoType";
+import { Filter } from "./filter/filterType";
 
 export const selectTodos = (state: AppState) => {
   const { todos, filter } = state;
+  const filterConstraints = getFilterConstraints(filter);
+  return todos.filter(filterConstraints);
+};
 
-  const filterConstraints = (todo: Todo) => {
+const getFilterConstraints = (filter: Filter) => {
+  // TODO: Add search to condition when filter state has search string
+  return (todo: Todo) => {
     if (!filter.showImportant && !todo.important) return false;
     if (!filter.showDone && todo.done) return false;
     return true;
   };
-
-  return todos.filter(filterConstraints);
 };
